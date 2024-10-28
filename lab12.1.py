f0f57f1eaacdc55375e691808427774c0760c488
tw1zzR <regenuss@gmail.com>
2024-10-28 14:12:13 +0200

    Сompleted LAB12.1

--- lab12.1.py
+++ lab12.1.py
@@ -4,12 +4,13 @@ def first_Fragments(text, N):
     for i in range(0, len(text), N):
         print(text[i:i + N])
 
-print("\nВикликаємо першу функцію:")
-first_Fragments("Давайте розділимо цей текст на фрагменти", 4)
-
 def scnd_Fragments(text):
     for i in range(0, len(text), 2):
         print(text[i:i + 2], random.randrange(9), random.randrange(9))
+
+
+print("\nВикликаємо першу функцію:")
+first_Fragments("Давайте розділимо цей текст на фрагменти", 4)
 
 print("\nВикликаємо другу функцію:")
 scnd_Fragments("Давайте розділимо цей текст на фрагменти")
