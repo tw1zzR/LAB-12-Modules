476908cd5522e7bbb4cf861e48391e4ad52a1e16
tw1zzR <regenuss@gmail.com>
2024-10-28 16:11:49 +0200

    Сompleted LAB12.3

---
+++ lab12.3.1.py
@@ -0,0 +1,9 @@
+import lab12module
+
+radius = 2
+
+result_volume = round(lab12module.volume_sphere(2),2)
+print(f"\nОбьєм кулі з радіусом {radius} становить: {result_volume}")
+
+result_square = round(lab12module.square_sphere(2),2)
+print(f"Площа поверхні кулі з радіусом {radius} становить: {result_square}")
---
+++ lab12module.py
@@ -0,0 +1,12 @@
+import math
+
+def volume_sphere(radius):
+    volume = (4/3) * math.pi * math.pow(radius,3)
+    return volume
+
+def square_sphere(radius):
+    square = 4 * math.pi * math.pow(radius,2)
+    return square
+
+
+
